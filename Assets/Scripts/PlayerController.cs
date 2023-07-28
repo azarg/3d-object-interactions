95e17319c369e0465c09f5ca1dd4de0c11a52387
azarg <azar.gurbanov@gmail.com>
2023-07-28 15:20:00 -0500

    collision detection

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,23 +6,57 @@ public class PlayerController : MonoBehaviour {
     [SerializeField] float turnSpeed = 15;
     [SerializeField] InputManager inputManager;
 
-    private bool isWalking;
+    private bool walkingAnimationEnabled;
+    private readonly float playerHeight = 2.8f;
+    private readonly float playerRadius = 0.5f;
 
     private void Update() {
         Vector2 inputVector = inputManager.GetMovementVectorNormalized();
-
-        var moveVector = new Vector3(inputVector.x, 0, inputVector.y);
-
-        if (moveVector != Vector3.zero) {
-            isWalking = true;
-            transform.position += moveVector * moveSpeed * Time.deltaTime;
-            transform.forward = Vector3.Slerp(transform.forward, moveVector, turnSpeed * Time.deltaTime);
-        } else {
-            isWalking = false;
-        }
+        HandleMovementAnimation(inputVector);
+        HandleMovement(inputVector);
     }
 
     public bool IsWalking() {
-        return isWalking;
+        return walkingAnimationEnabled;
+    }
+
+    private void HandleMovementAnimation(Vector2 inputVector) {
+        walkingAnimationEnabled = inputVector != Vector2.zero;
+    }
+
+    private void HandleMovement(Vector2 inputVector) {
+        // no need for movement handling if there is no input
+        if (inputVector == Vector2.zero) return;
+
+        // calculate move vector and turn in that direction
+        var moveDirection = new Vector3(inputVector.x, 0, inputVector.y);
+        transform.forward = Vector3.Slerp(transform.forward, moveDirection, turnSpeed * Time.deltaTime);
+
+        // if cannot move in the direction of input vector, try moving horizontally or vertailly
+        float moveDistance = moveSpeed * Time.deltaTime;
+        bool canMove = CanMove(moveDirection, moveDistance);
+        if (!canMove) {
+            
+            var moveDirectionX = new Vector3(inputVector.x, 0, 0);
+            canMove = CanMove(moveDirectionX, moveDistance) ;
+            if (canMove) {
+                moveDirection = moveDirectionX;
+            }
+            else {
+                var moveDirectionY = new Vector3(0, 0, inputVector.y);
+                canMove = CanMove(moveDirectionY, moveDistance);
+                if (canMove) {
+                    moveDirection = moveDirectionY;
+                }
+            }
+        }
+
+        if (canMove) {
+            transform.position += moveDirection * moveDistance;
+        }
+    }
+
+    private bool CanMove(Vector3 moveDirection, float maxDistance) {
+        return !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirection, maxDistance);
     }
 }
